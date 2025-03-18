98c6d7d6da2fc6f08d3ebe5e604c1bd06a92ac96
First <pyimcha@cmkl.ac.th>
2025-03-18 14:59:34 +0700

    Add Login Functionality

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,19 @@
 import javax.swing.*;
-import java.awt.*;
+import java.util.ArrayList;
 
-public class Main
-{
-    public static void main(String[] args)
-    {
-        new MainFrame();
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<User> users = new ArrayList<>();
+
+        ArrayList<String> genres = new ArrayList<>();
+        genres.add("Science Fiction");
+        genres.add("Fantasy");
+        genres.add("Comedy");
+        User user1 = new User("First", "First@gmail.com", "1234", genres);
+        users.add(user1);
+
+
+        MainFrame mainFrame = new MainFrame(users);
+        mainFrame.setVisible(true);
     }
 }
