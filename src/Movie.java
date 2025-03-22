0f5f8fbba73e1fdf449b11bda7dca423701480cf
First <pyimcha@cmkl.ac.th>
2025-03-22 20:03:43 +0700

    Add movie class

---
+++ src/Movie.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+
+public class Movie
+{
+    private int movieID;
+    private String title;
+    private String genre;
+
+    public Movie(int movieID, String title, String genre)
+    {
+        this.movieID = movieID;
+        this.title = title;
+        this.genre = genre;
+    }
+
+    public int getMovieID()
+    {
+        return movieID;
+    }
+
+    public void setMovieID(int movieID)
+    {
+        this.movieID = movieID;
+    }
+
+    public String getMovieTitle()
+    {
+        return title;
+    }
+
+    public void setMovieTitle(String title)
+    {
+        this.title = title;
+    }
+
+    public String getGenre()
+    {
+        return genre;
+    }
+
+    public void setGenre(String genre)
+    {
+        this.genre = genre;
+    }
+
+}
